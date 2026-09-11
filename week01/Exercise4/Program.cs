03ac2846000e05a8618205c5982a5f46bbd1fa0d
Michael Taggart <mtaggarthawk@gmail.com>
2026-09-10 22:35:15 -0600

    exercise 4

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -1,9 +1,44 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<double> numberList = new List<double>();
+
+        double userInput = 1;
+
+        while (userInput != 0)
+        {
+            Console.Write("Enter Number: ");
+            userInput = double.Parse(Console.ReadLine());
+
+            if (userInput != 0)
+            {
+                numberList.Add(userInput);
+            }
+        }
+
+        double sum = 0;
+        double average = 0;
+        double largestNumber = 0;        
+
+        // Console.WriteLine(numberList.Count);
+
+
+        foreach (double number in numberList)
+        {
+            sum = number + sum;
+        }
+
+        average = sum / numberList.Count;
+
+        largestNumber = numberList.Max();
+
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largestNumber}");
+
     }
 }
