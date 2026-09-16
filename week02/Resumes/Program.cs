81edca61033c5f2ee08fac7fe6a8d40bb665d170
Michael Taggart <mtaggarthawk@gmail.com>
2026-09-16 03:00:40 -0600

    Week 2 learning activity

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -18,7 +18,7 @@ class Program
         job2._endYear = 2023;
 
         Resume myResume = new Resume();
-        myResume._name = "Allison Rose";
+        myResume._name = "Michael Taggart";
 
         myResume._jobs.Add(job1);
         myResume._jobs.Add(job2);
